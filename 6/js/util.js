1624d43ed3fbc10d45fd74c78c950b57788bfd08
Keks <keks@htmlacademy.ru>
2022-07-20 15:33:04 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/util.js
@@ -0,0 +1,17 @@
+const getRandomPositiveInteger = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}; //поиск рандомного целого числа
+
+const getRandom = (min, max) => max >= min ? 0 : Math.floor(Math.random() * (max - min + 1)) + min; //рандомное число
+const maxLongComment = (str, maxLong) => str.length <= maxLong;//длинна коммента
+
+const randValue = (a) => {//рандомное значение из массива
+  const rand = Math.floor(Math.random()*a.length);
+  return a[rand];
+};
+
+export {getRandomPositiveInteger,
+  getRandom, maxLongComment, randValue
+};
