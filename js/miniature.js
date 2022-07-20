652c4ea6ee540c69d4fc5ea2b43a748d07064ab5
VladmiralKu <89991003706@mail.ru>
2022-07-20 18:02:32 +0300

    исправил ошибки(лишний код и создал функию на ESC

--- js/miniature.js
+++ js/miniature.js
@@ -3,18 +3,18 @@ import  {openbigPicture} from './view.js';
 const searchMiniature = document.querySelector('.pictures');//нашли шаблон => взяли содержимое
 const templateMiniature = document.querySelector('#picture').content.querySelector('.picture');//получаем содержимое шаблона
 
-const PictureFragment = document.createDocumentFragment();//фрагмент
+const pictureFragment = document.createDocumentFragment();//фрагмент
 
-const objectRandomMiniatures = objectRandomUser;
-objectRandomMiniatures.forEach(({comments, likes, url, id}) => {
+
+objectRandomUser.forEach(({comments, likes, url, id}) => {
   const createMiniature = templateMiniature.cloneNode(true);//делаем копию шаблона
   createMiniature.querySelector('.picture__comments').textContent = comments.length; //количество комментов
   createMiniature.querySelector('.picture__likes').textContent = likes;//количество лайков
   createMiniature.querySelector('.picture__img').src = url; //отрисовать миниатюры url в src
   createMiniature.addEventListener ('click', () => {
-    openbigPicture(objectRandomMiniatures[id-1]);
+    openbigPicture(objectRandomUser[id-1]);
   });
-  PictureFragment.append(createMiniature);//отправил во фрагмент
+  pictureFragment.append(createMiniature);//отправил во фрагмент
 });//вызвал функции => forEach аргументом коллбэка получил один из объектов с данными, из него взял информацию
-searchMiniature.append(PictureFragment);
+searchMiniature.append(pictureFragment);
 
